9149ee3ed62b8e9f4e01836ea4fbee597602996f
Katie Smith <ks@katchecode.com>
2018-10-05 10:01:11 -0600

    WIP beginnings of login page

--- lib/account_login_page.dart
+++ lib/account_login_page.dart
@@ -36,74 +36,86 @@ class _AccountLoginPageState extends State<AccountLoginPage> {
   Widget build(BuildContext context) {
 
     return Scaffold(
-      appBar: new AppBar(
-        title: new Text('Login'),
-      ),
       body: Container(
+        decoration: new BoxDecoration(
+          image: new DecorationImage(
+            image: new AssetImage("images/background.jpg"),
+            fit: BoxFit.cover,
+          ),
+        ),
         padding: new EdgeInsets.all(30.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              TextFormField(
-                autofocus: true,
-                decoration: InputDecoration(
-                  labelText: 'Username',
-                ),
-                validator: (value) {
-                  if (value.isEmpty) {
-                    return 'Please your username.';
+        child: Container(
+          decoration: new BoxDecoration(
+            image: new DecorationImage(
+              image: new AssetImage("images/ticket.png"),
+              fit: BoxFit.contain,
+            ),
+          ),
+          padding: new EdgeInsets.only(top: 350.0, right: 220.0, bottom: 20.0, left: 210.0,),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                TextFormField(
+                  autofocus: true,
+                  decoration: InputDecoration(
+                    labelText: 'Username',
+                  ),
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Please your username.';
+                    }
+                  },
+                  onSaved: (String value) {
+                    _login.username = value;
                   }
-                },
-                onSaved: (String value) {
-                  _login.username = value;
-                }
-              ),
-              TextFormField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  labelText: 'Password'
                 ),
-                validator: (value) {
-                  if (value.isEmpty) {
-                    return 'Please enter your password.';
+                TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: 'Password'
+                  ),
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Please enter your password.';
+                    }
+                  },
+                  onSaved: (String value) {
+                    _login.password = value;
                   }
-                },
-                onSaved: (String value) {
-                  _login.password = value;
-                }
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 16.0),
-                    child: RaisedButton(
-                      onPressed: () {_accountLogin(_formKey.currentState);},
-                      child: Text(
-                        'Login',
-                        style: TextStyle(
-                          color: Colors.white,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: RaisedButton(
+                        onPressed: () {_accountLogin(_formKey.currentState);},
+                        child: Text(
+                          'Login',
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 16.0),
-                    child: RaisedButton(
-                      onPressed: () {_accountRegister(_formKey.currentState);},
-                      child: Text(
-                        'Register',
-                        style: TextStyle(
-                          color: Colors.white,
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: RaisedButton(
+                        onPressed: () {_accountRegister(_formKey.currentState);},
+                        child: Text(
+                          'Register',
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                ]
-              )
-            ],
+                  ]
+                )
+              ],
+            ),
           ),
         ),
       ),
