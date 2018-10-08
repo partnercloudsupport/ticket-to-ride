e9b3ff89505c86854e64e537dd8c8960d651a3e1
Landon Smith <ls@katchecode.com>
2018-10-08 10:50:15 -0600

    improve client-side and networking errors in api proxy. Update proxy ip to master server

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -13,9 +13,9 @@ export 'auth.pb.dart';
 export 'game.pb.dart';
 export 'health.pb.dart';
 
-final authProxy = AuthServiceApi(ClientProxy("auth", "http://35.188.56.148/"));
-final gameProxy = GameServiceApi(ClientProxy("game", "http://35.188.56.148/"));
-final healthProxy = HealthServiceApi(ClientProxy("health", "http://35.188.56.148/"));
+final authProxy = AuthServiceApi(ClientProxy("auth", "http://104.198.214.161/"));
+final gameProxy = GameServiceApi(ClientProxy("game", "http://104.198.214.161/"));
+final healthProxy = HealthServiceApi(ClientProxy("health", "http://104.198.214.161/"));
 
 class ClientProxy extends RpcClient {
 
@@ -26,19 +26,24 @@ class ClientProxy extends RpcClient {
 
   @override
   Future<T> invoke<T extends GeneratedMessage>(ClientContext ctx, String serviceName, String methodName, GeneratedMessage req, T emptyResponse) async {
-    var request = Request();
-    request.method = methodName;
-    request.service = package + "." + serviceName;
-    request.payload = req.writeToBuffer();
-    print(request.service);
-    var httpResponse = await http.post(url, body: request.writeToBuffer());
-    var response = Response.fromBuffer(httpResponse.bodyBytes);
-    if (response.code != Response_Code.OK) {
-      throw ApiError(response.code, response.message);
-    }
+    try {
+      var request = Request();
+      request.method = methodName;
+      request.service = package + "." + serviceName;
+      request.payload = req.writeToBuffer();
+      print(request.service);
+      var httpResponse = await http.post(url, body: request.writeToBuffer());
+      var response = Response.fromBuffer(httpResponse.bodyBytes);
+      if (response.code != Response_Code.OK) {
+        throw ApiError(response.code, response.message);
+      }
 
-    emptyResponse.mergeFromBuffer(response.payload);
-    return emptyResponse;
+      emptyResponse.mergeFromBuffer(response.payload);
+      return emptyResponse;
+    }
+    catch (err) {
+      throw ApiError(Response_Code.UNAVAILABLE, err.toString());
+    }
   }
 }
 
