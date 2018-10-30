09904937d9c975f6bd23d35368ef53a96efcf4fa
Katie Smith <ks@katchecode.com>
2018-10-29 23:37:22 -0600

    cleaning up main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:ticket_to_ride/theme/theme.dart';
-// import 'package:ticket_to_ride/global_context_widget.dart';
 
 import 'package:ticket_to_ride/fragments/account_login_fragment.dart';
 import 'package:ticket_to_ride/fragments/lobby_view_fragment.dart';
@@ -17,31 +16,26 @@ class TicketToRideApp extends StatelessWidget {
 
   final GlobalKey<NavigatorState> navigatorKey = new GlobalKey<NavigatorState>();
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    // return GlobalContextDEPR(
-      // child: new
-      return MaterialApp(
-        title: 'Ticket to Ride',
-        theme: ticketToRideTheme,
-        initialRoute: '/',
-        navigatorKey: navigatorKey,
-        // home: new AccountLoginFragment(navigatorKey, title: 'Login'),
-        routes: <String, WidgetBuilder>{
-         '/': (BuildContext context) {
-            var fragment = new AccountLoginFragment(navigatorKey, title: 'Login');
-            fragment.addObserver(AccountLoginPresenter(fragment));
-            return fragment;
-          },
-         //'/game_selection': (BuildContext context) => ,
-         '/lobby_view': (BuildContext context) {
-            var fragment = new LobbyViewFragment(navigatorKey, title: 'Login');
-            fragment.addObserver(LobbyViewPresenter(fragment));
-            return fragment;
-          },
+    return MaterialApp(
+      title: 'Ticket to Ride',
+      theme: ticketToRideTheme,
+      initialRoute: '/',
+      navigatorKey: navigatorKey,
+      routes: <String, WidgetBuilder>{
+       '/': (BuildContext context) {
+          var fragment = new AccountLoginFragment(navigatorKey, title: 'Login');
+          fragment.addObserver(AccountLoginPresenter(fragment));
+          return fragment;
         },
-      // )
+       //'/game_selection': (BuildContext context) => ,
+       '/lobby_view': (BuildContext context) {
+          var fragment = new LobbyViewFragment(navigatorKey, title: 'Login');
+          fragment.addObserver(LobbyViewPresenter(fragment));
+          return fragment;
+        },
+      },
     );
   }
 }
