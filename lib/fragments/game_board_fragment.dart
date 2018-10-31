af227d9d611d3a2de658d6a7e6b20bb5e9cc5c45
Katie Smith <ks@katchecode.com>
2018-10-30 20:27:12 -0600

    setting up fragments for game view

---
+++ lib/fragments/game_board_fragment.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+
+// class Player {
+//   String name;
+//   String color;
+//
+//   Player(this.name, this.color);
+// }
+//
+// class LobbyGame {
+//   String name;
+//   String hostName;
+//   List players = [];
+//   bool canStart = false;
+// }
+
+abstract class GameBoardObserver {
+  claimRoute() {}
+}
+
+class GameBoardFragment extends StatefulWidget {
+  GameBoardFragment(this.navKey, {Key key, this.title}) : super(key: key);
+
+  final GlobalKey<NavigatorState> navKey;
+  final observers = List<GameBoardObserver>();
+  final String title;
+
+  void addObserver(GameBoardObserver o) {
+    observers.add(o);
+  }
+
+  void removeObserver(GameBoardObserver o) {
+    observers.remove(o);
+  }
+
+  void navigatePush(String routeName) {
+    navKey.currentState.pushNamed(routeName);
+  }
+
+  void navigatePop() {
+    navKey.currentState.pop();
+  }
+
+  @override
+  _GameBoardFragmentState createState() => new _GameBoardFragmentState();
+}
+
+class _GameBoardFragmentState extends State<GameBoardFragment> {
+
+  _buildCities() {
+    double width = MediaQuery.of(context).size.width * .75;
+    double height = MediaQuery.of(context).size.height * .70;
+
+    var cities = [[.10, .3], [.5, .5], [.75, .05]];
+
+    return cities.map((point) {
+      return Positioned(
+        left: width * point[0],
+        top: height * point[1],
+        child: Container(
+          width: 20.0,
+          height: 20.0,
+          decoration: BoxDecoration(
+            color: const Color(0xff000000),
+            borderRadius: new BorderRadius.circular(25.0),
+          ),
+        ),
+      );
+    }).toList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width * .75;
+    double height = MediaQuery.of(context).size.height * .70;
+
+    return new Stack(
+        children: List.from([
+          Container(
+            width: width,
+            height: height,
+            decoration: new BoxDecoration(
+              image: new DecorationImage(
+                image: new AssetImage("images/map.png"),
+                fit: BoxFit.cover,
+              ),
+            ),
+          )
+        ])..addAll(_buildCities())
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,16 @@ import 'package:ticket_to_ride/theme/theme.dart';
 
 import 'package:ticket_to_ride/fragments/account_login_fragment.dart';
 import 'package:ticket_to_ride/fragments/lobby_view_fragment.dart';
+import 'package:ticket_to_ride/fragments/game_view_fragment.dart';
+import 'package:ticket_to_ride/fragments/game_board_fragment.dart';
+import 'package:ticket_to_ride/fragments/game_player_fragment.dart';
+import 'package:ticket_to_ride/fragments/game_menu_fragment.dart';
+import 'package:ticket_to_ride/fragments/game_bank_fragment.dart';
+import 'package:ticket_to_ride/fragments/game_hand_fragment.dart';
 
 import 'package:ticket_to_ride/presenters/account_login_presenter.dart';
 import 'package:ticket_to_ride/presenters/lobby_view_presenter.dart';
+// import 'package:ticket_to_ride/presenters/game_view_presenter.dart';
 // import 'package:ticket_to_ride/presenters/game_selection_presenter.dart';
 
 void main() async {
@@ -21,12 +28,12 @@ class TicketToRideApp extends StatelessWidget {
     return MaterialApp(
       title: 'Ticket to Ride',
       theme: ticketToRideTheme,
-      initialRoute: '/',
+      initialRoute: '/game_view',
       navigatorKey: navigatorKey,
       routes: <String, WidgetBuilder>{
        '/': (BuildContext context) {
           var fragment = new AccountLoginFragment(navigatorKey, title: 'Login');
-          fragment.addObserver(AccountLoginPresenter(fragment));
+          fragment.addObserver(AccountLoginPresenter(fragment, navigatorKey));
           return fragment;
         },
        //'/game_selection': (BuildContext context) => ,
@@ -35,6 +42,25 @@ class TicketToRideApp extends StatelessWidget {
           fragment.addObserver(LobbyViewPresenter(fragment));
           return fragment;
         },
+        '/game_view': (BuildContext context) {
+          var boardFragment = new GameBoardFragment(navigatorKey);
+          var playerFragment = new GamePlayerFragment(navigatorKey);
+          var menuFragment = new GameMenuFragment(navigatorKey);
+          var bankFragment = new GameBankFragment(navigatorKey);
+          var handFragment = new GameHandFragment(navigatorKey);
+
+          var fragment = new GameViewFragment(
+            navigatorKey,
+            boardFragment: boardFragment,
+            playerFragment: playerFragment,
+            menuFragment: menuFragment,
+            bankFragment: bankFragment,
+            handFragment: handFragment,
+
+            title: 'Login');
+          // fragment.addObserver(LobbyViewPresenter(fragment));
+          return fragment;
+         },
       },
     );
   }
