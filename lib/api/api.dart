abf44c4b6fc003c505a4a296dfac4cfd28489335
Landon Smith <ls@katchecode.com>
2019-01-24 19:00:04 -0700

    this is stuff I forgot to commit, I don't remember what it is

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -15,8 +15,8 @@ export 'card.pb.dart';
 export 'route.pb.dart';
 export 'api_error.dart';
 
-final url = "http://dev.wwttr.umkhandi.com/";
-// final url = "http://localhost:8080/";
+// final url = "http://dev.wwttr.umkhandi.com/";
+final url = "http://10.228.155.142:8080/";
 
 final authProxy = AuthServiceProxy(url);
 final gameProxy = GameServiceProxy(url);
